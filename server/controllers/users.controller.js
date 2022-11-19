4eddfe08be67eb8195d7936aade83861316efeea
vDemian0w00 <cfajardoa2000@alumno.ipn.mx>
2022-11-19 03:52:56 -0600

    conexion sql

--- server/controllers/users.controller.js
+++ server/controllers/users.controller.js
@@ -5,42 +5,36 @@ export const getUsers = async (req, res) => {
 };
 
 export const createUser = async (req, res) => {
-  const { firstname, lastname, email, password, confirmpass } = req.body;
+  const { firstName, lastName, email, password, confirmPassword, image } =
+    req.body;
 
-  if (!firstname || !lastname || !email || !password || !confirmpass) {
+  if (!firstName || !lastName || !email || !password || !confirmPassword) {
     res.status(400).send("Todos los campos son obligatorios");
   }
-  if (password !== confirmpass) {
+  if (password !== confirmPassword) {
     res.status(400).send("Las contraseñas no coinciden");
   }
 
-  const [rows] = await pool.query("SELECT * FROM usuarios WHERE correoUsu = ?", [email]);
-  if(rows.length > 0) {
-    req.session.data = {message: "El correo ya está registrado"};
-    req.session.save((err)=>{
-      if(!err){
-        res.redirect("/login");
-      }else{
-        res.status(500).send("Error al guardar la sesión");
-      }
-    })
-    return;
+  const [rows] = await pool.query("SELECT * FROM usuarios WHERE correoUsu = ?", [
+    email,
+  ]);
+  if (rows.length > 0) {
+    return res.send({ message: "El correo ya está registrado", status: 400 });
   }
 
   const result = await pool.query(
     "INSERT INTO usuarios(nombreUsu, apellidosUsu, correoUsu, contraseñaUsu, imgurlUsu, rolUsu) values (?, ?, ?, ?, ?, ?)",
-    [firstname, lastname, email, password, req.file.filename, "alumno"]
-  );
-
-  req.session.data = { message: "Para continuar, Inicie Sesión" };
-
-  req.session.save((err) => {
-    if (!err) {
-      res.redirect("/login");
-    } else {
-      res.status(400).send("Error al crear usuario");
-    }
+    [firstName, lastName, email, password, image, "alumno"]
+  ).catch((err) => {
+    console.log(err);
   });
+
+  if (result) {
+    return res.send({ message: "Usuario creado", status: 200 });
+  }else{
+    return res.send({ message: "Error al crear usuario, por favor intente mas tarde", status: 400 });
+  }
+
 };
 
 export const loginUser = async (req, res) => {
@@ -49,7 +43,10 @@ export const loginUser = async (req, res) => {
     res.status(400).send("Todos los campos son obligatorios");
   }
 
-  const [rows] = await pool.query("SELECT * FROM usuarios WHERE correoUsu = ? and contraseñaUsu = ?;", [correo, contraseña]);
+  const [rows] = await pool.query(
+    "SELECT * FROM usuarios WHERE correoUsu = ? and contraseñaUsu = ?;",
+    [correo, contraseña]
+  );
 
   if (rows.length > 0) {
     req.session.user = rows[0];
@@ -61,7 +58,7 @@ export const loginUser = async (req, res) => {
       }
     });
   } else {
-    req.session.data = { message: "Correo o contraseña incorrectos"};
+    req.session.data = { message: "Correo o contraseña incorrectos" };
     req.session.save((err) => {
       if (!err) {
         res.redirect("/login");
